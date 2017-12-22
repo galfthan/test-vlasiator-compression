4200fc94b79da027da2bec7321d5e4594d4ab683
Sebastian von Alfthan <galfthan@gmail.com>
2017-12-22 23:40:26 +0200

    Changed to RMS measure for relative error. Also only look at velocity cells above min_val

--- test-pack.py
+++ test-pack.py
@@ -10,63 +10,69 @@ cells = np.sort(f.read_variable("CellID"))
 
 rates=[8, 16, 24, 32]
 
-print "#cellid blocks sum float float-scaled half-scaled zfp-8 zfp-16 zfp-24 zfp-32"
+print "#cellid blocks sum float float2 half2 zfp-8 zfp-16 zfp-24 zfp-32"
 for cellid in cells:
     
     velspace = f.read_blocks(cellid)
     if len(velspace) == 0:
         continue
-
-    tot_error_float=np.empty(0, dtype = np.float64)
-    tot_error_float2=np.empty(0, dtype = np.float64)
-    tot_error_half2=np.empty(0, dtype = np.float64)
-    tot_error_zfp =  {}
+    block_zfp_d =  {}
+    rms_zfp={}
     
     for rate in rates:
-        tot_error_zfp[rate] = np.empty(0, dtype = np.float64)
-
-    
+        rms_zfp[rate] = 0.0
+        block_zfp_d[rate] = np.empty(64, dtype=np.float64)
+        
     cell_sum = 0
+    vcells_counted=0
+    min_val=1e-20
+    rms_float = 0.0
+    rms_float2 = 0.0
+    rms_half2 = 0.0
+    
     for block in velspace[1]:
         block_max=np.max(block)
         block_min=np.min(block)
         block_sum=np.sum(block)
         cell_sum = cell_sum + block_sum
-        
+
+        #compress - decompress by casting, with or without some scaling
         block_float=block.astype(np.float32)
         block_float2=(np.finfo(np.float32).max * (block / block_max)).astype(np.float32)
         block_half2=(np.finfo(np.float16).max * (block / block_max)).astype(np.float16)
-
         block_float_d=block_float.astype(np.float64)
         block_float2_d=block_float2.astype(np.float64)/ np.finfo(np.float32).max * block_max
         block_half2_d=block_half2.astype(np.float64)/ np.finfo(np.float16).max * block_max
 
+        #compress  - decompress with ZFP
         for rate in rates:
-            block_zfp_d = np.empty(64, dtype=np.float64)
-            zfp.compress_block(block, block_zfp_d, rate)
-            tot_error_zfp[rate] = np.append(tot_error_zfp[rate], np.abs(block - block_zfp_d))            
+            block_zfp_d[rate] = np.empty(64, dtype=np.float64)
+            zfp.compress_block(block, block_zfp_d[rate], rate)
+            
+        #compute root mean squares
+        for i in range(64):
+            if block[i] > min_val:
+                vcells_counted = vcells_counted + 1
+                rms_float = rms_float + ((block[i] - block_float_d[i])/block[i])**2
+                rms_float2 = rms_float2 + ((block[i] - block_float2_d[i])/block[i])**2
+                rms_half2 = rms_half2 + ((block[i] - block_half2_d[i])/block[i])**2
 
-        tot_error_float = np.append(tot_error_float, np.abs(block - block_float_d))
-        tot_error_float2 = np.append(tot_error_float2, np.abs(block - block_float2_d))
-        tot_error_half2 = np.append(tot_error_half2, np.abs(block - block_half2_d))
+        for rate in rates:
+            for i in range(64):
+                if block[i] > min_val:
+                    rms_zfp[rate] = rms_zfp[rate] + ((block[i] - block_zfp_d[rate][i])/block[i])**2
 
-
+                    
+ 
     print cellid, len(velspace[1]), cell_sum, \
-        np.median(tot_error_float), \
-        np.median(tot_error_float2), \
-        np.median(tot_error_half2), \
-        np.median(tot_error_zfp[8]), \
-        np.median(tot_error_zfp[16]), \
-        np.median(tot_error_zfp[24]), \
-        np.median(tot_error_zfp[32])
-
+        np.sqrt(rms_float/vcells_counted), \
+        np.sqrt(rms_float2/vcells_counted), \
+        np.sqrt(rms_half2/vcells_counted), \
+        np.sqrt(rms_zfp[8]/vcells_counted), \
+        np.sqrt(rms_zfp[16]/vcells_counted), \
+        np.sqrt(rms_zfp[24]/vcells_counted), \
+        np.sqrt(rms_zfp[32]/vcells_counted)
     
-
-    # print "Tot err float  avg, max, min, median:",  np.average(tot_error_float), np.max(tot_error_float), np.min(tot_error_float), np.median(tot_error_float)
-    # print "Tot err float-scaled 32  avg, max, min, median:",  np.average(tot_error_float2), np.max(tot_error_float2), np.min(tot_error_float2), np.median(tot_error_float2)
-    # print "Tot err half-scaled 16  avg, max, min, median:",  np.average(tot_error_half2), np.max(tot_error_half2), np.min(tot_error_half2), np.median(tot_error_half2)
-    # for rate in rates:
-    #     print "Tot err zfp",rate,"  avg, max, min, median:",  np.average(tot_error_zfp[rate]), np.max(tot_error_zfp[rate]), np.min(tot_error_zfp[rate]), np.median(tot_error_zfp[rate])
 
 
 
